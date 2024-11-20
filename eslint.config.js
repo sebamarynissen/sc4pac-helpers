c4fc3177abf23dbf029dda8ec2e541eb940a5b63
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-20 11:35:05 +0100

    Automatically parse sc4e dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import wh from '@whisthub/eslint-config/flat';
 export default [
 	wh,
 	{
-		files: ["lib/data.js"],
+		files: ["lib/data.js", "lib/data/**"],
 		rules: {
 			"sort-keys": ["error", "asc", { "natural": true }],
+			"quotes": "off",
 		},
 	},
 ];
