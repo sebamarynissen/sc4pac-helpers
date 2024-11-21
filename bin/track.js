af295b3ef87e9e852344ea20527a1bb2951c2b79
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-21 10:56:03 +0100

    Update track.js

--- bin/track.js
+++ bin/track.js
@@ -1,8 +1,6 @@
-import chalk from 'chalk';
 import DependencyTracker from '../lib/dependency-tracker.js';
 
 const tracker = new DependencyTracker();
 const sources = process.argv.slice(2);
 const result = await tracker.track(sources);
 result.dump();
-// deps.forEach(dep => console.log(chalk.cyan(dep)));
