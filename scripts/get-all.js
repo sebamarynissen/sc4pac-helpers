36cb0c2d325ef2327447a3a35c68837305ce6319
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-11-23 10:19:06 +0100

    Add script for listing all packages in a folder

---
+++ scripts/get-all.js
@@ -0,0 +1,24 @@
+// # get-all.js
+import { Glob } from 'glob';
+import { parseAllDocuments } from 'yaml';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const folder = path.resolve(import.meta.dirname, '../zasco/src/jasoncw');
+console.log(folder);
+const glob = new Glob('**/*.yaml', {
+	cwd: folder,
+	absolute: true,
+});
+
+let arr = [];
+for (let file of glob) {
+	let buffer = fs.readFileSync(file);
+	let docs = parseAllDocuments(buffer+'');
+	for (let doc of docs) {
+		let json = doc.toJSON();
+		if (!json.group) continue;
+		arr.push(`${json.group}:${json.name}`);
+	}
+}
+console.log(JSON.stringify(arr, null, 2));
