c908c8a9126a8cf9dc6b50a096ece1b8f77cd470
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-02 23:13:56 +0100

    Collect girafe trees
    We've started refactoring the way we perform our automatic tree patching. The idea here is that we build up an entire tree database first - which is committed to version control - and then we can use this tree database to perform the patching. This decouples the discovery of the models from the patching itself, which should make the code more readable.

---
+++ scripts/build-girafe-database.js
@@ -0,0 +1,246 @@
+// # build-girafe-database.js
+import path from 'node:path';
+import { Document } from 'yaml';
+import { Glob } from 'glob';
+import { DBPF, ExemplarProperty } from 'sc4/core';
+
+// # run()
+// Entry point for building up the Girafe database.
+async function run() {
+	const glob = new Glob('*/', {
+		cwd: path.resolve(import.meta.dirname, '../packages/Girafe'),
+		absolute: true,
+	});
+
+	const db = {};
+	for await (let pkg of glob) {
+		await handlePackage(db, pkg);
+	}
+	return db;
+
+}
+
+// # async handlePackage(db, dir)
+async function handlePackage(db, dir) {
+	
+	// Find all files in this package and get the exemplars.
+	const glob = new Glob('*.dat', {
+		cwd: dir,
+		absolute: true,
+	});
+	let files = await glob.walk();
+	let exemplars = files.map(file => new DBPF(file).exemplars).flat();
+
+	// Loop all exemplars and handle props and flora differently.
+	for (let entry of exemplars) {
+		let exemplar = entry.read();
+		let type = exemplar.singleValue(ExemplarProperty.ExemplarType);
+		switch (type) {
+			case 0x0f: await handleFlora(db, entry); break;
+			case 0x1e: await handleProp(db, entry); break;
+		}
+	}
+
+}
+
+// # handleProp(entry)
+// Processes the given prop exemplar and adds it to the database.
+function handleProp(db, entry) {
+	let exemplar = entry.read();
+	let seasons = getPropSeasons(exemplar, entry);
+	let id = getPropId(exemplar, entry);
+	let object = db[id] ??= {
+		type: 'prop',
+		seasons: {},
+	};
+	Object.assign(object.seasons, seasons);
+}
+
+// # handleFora(entry)
+// Processes the given flora exemplar and adds it to the database.
+function handleFlora(db, entry) {
+	let exemplar = entry.read();
+	let seasons = getFloraSeasons(exemplar, entry);
+	let id = getFloraId(exemplar, entry);
+	let object = db[id] ??= {
+		type: 'flora',
+		seasons: {},
+	};
+	Object.assign(object.seasons, seasons);
+}
+
+// # getPropTreeId(exemplar, entry)
+// Determines the tree id of the given prop exemplar. A tree id is a way to 
+// uniquely identify a tree regardless of the various seasons it has models for. 
+// In other words, every tree id will have a set of season that it supports.
+const seasonRegex = /(autumn|summer|winter|evergreen|spring|seasonal)/i;
+function getPropId(exemplar, entry) {
+	let tree = exemplar
+		.singleValue(ExemplarProperty.ExemplarName)
+		.replace(seasonRegex, '')
+		.replace(/^Grfe_/g, '')
+		.replace(/_$/, '')
+		.replaceAll(/_+/g, '-')
+		.toLowerCase();
+	return `girafe:${tree}-prop`;
+}
+
+// # getPropSeasons(exemplar, entry)
+// Returns all model TGIs that we can find in this prop exemplar. Note that 
+// Girafe's prop exemplars typically don't contain multiple seasons.
+function getPropSeasons(exemplar, entry) {
+
+	// If the prop only has an RKT1, then it typically is a summer prop because 
+	// it is always visible.
+	let rkt1 = exemplar.get(ExemplarProperty.ResourceKeyType1);
+	if (rkt1) {
+		return { summer: rkt1.slice(1) };
+	}
+
+	// If the prop has an RKT4 of 8 reps - always the case for Girafe - then 
+	// we'll determine the season based on the simulator date start.
+	let rkt4 = exemplar.get(ExemplarProperty.ResourceKeyType4);
+	if (rkt4) {
+		let model = rkt4.slice(6);
+		let season = getSeasonFromStartDate(exemplar);
+		return { [season]: model };
+	}
+
+	// By default, nothing is found.
+	return {};
+
+}
+
+function getSeasonFromStartDate(exemplar) {
+	let [month] = exemplar.get(ExemplarProperty.SimulatorDateStart) || [0, 0];
+	switch (month) {
+		case 0: return 'summer';
+		case 1: return 'winter';
+		case 2: return 'winter';
+		case 3: return 'summer';
+		case 4: return 'summer';
+		case 5: return 'summer';
+		case 6: return 'summer';
+		case 7: return 'summer';
+		case 8: return 'summer';
+		case 9: return 'fall';
+		case 10: return 'fall';
+		case 11: return 'fall';
+		case 12: return 'winter';
+	}
+}
+
+// # getFloraId(exemplar, entry)
+// Parses the unique id for this flora regardless of seasons. Note that serbian 
+// spruces, abies grandis and maples need a special treatment because there are 
+// multiple versions that might clash.
+function getFloraId(exemplar, entry) {
+	let pkg = path.basename(path.dirname(entry.dbpf.file));
+	let suffix = '';
+	let [group, name, version] = pkg.split('.');
+	if (pkg.match(/(serbian|maples|abies)/)) {
+		suffix = `-v${version}`;
+	}
+	let [, variant = ''] = exemplar
+		.get(ExemplarProperty.ExemplarName)
+		.match(/_([A-Z]\d?|\d{2})$/) || [];
+	if (variant) {
+		variant = variant.replace(/([A-Z])\d$/, '$1');
+		variant = `-${variant}`.toLowerCase();
+	}
+	return `${group}:${name}${suffix}${variant}-flora`;
+}
+
+// # getFloraSeasons(exemplar, entry)
+// Extracts the models for every season from the flora exemplar.
+function getFloraSeasons(exemplar, entry) {
+
+	// RKT5 flora is older flora where the models for the various seasons are 
+	// determined by an offset.
+	let rkt5 = exemplar.get(ExemplarProperty.ResourceKeyType5);
+	if (rkt5) {
+		let [group, instance] = rkt5.slice(1);
+		return {
+			fall: [group, instance],
+			winter: [group, instance + 0x00010000],
+			summer: [group, instance + 0x00020000],
+		};
+	}
+
+	// In case the flora has an RKT4, then we read the various season models 
+	// from the exemplar itself.
+	let rkt4 = exemplar.get(ExemplarProperty.ResourceKeyType4);
+	if (rkt4) {
+		let n = rkt4.length / 8;
+		let seasons = {};
+		for (let i = 0; i < n; i++) {
+			let index = 8*i;
+			let season = ['fall', 'winter', 'summer'][rkt4[index]];
+
+			// Some trees are snow-covered in winter. However, we want them to 
+			// have their normal summer appearance in winter - these are 
+			// coniferous trees - so we rename the season to "snow" in that case/
+			if (season === 'winter') {
+				let pkg = path.basename(path.dirname(entry.dbpf.file));
+
+				// Larches come in two variants: snow and leafless winter. Hence 
+				// we have to find the proper model.
+				if (pkg.includes('larches')) {
+					if (entry.dbpf.file.match(/_S\.dat$/)) {
+						season = 'snow';
+					}
+				} else if (hasSnow(pkg)) {
+					season = 'snow';
+				}
+			}
+			let model = rkt4.slice(index+6, index+8);
+			seasons[season] = model;
+
+		}
+		return seasons;
+
+	}
+
+	// If the model is using an RKT1, then it's an evergreen model or everwinter 
+	// model. We don't use those because we already read the models here from 
+	// the seasonal ones.
+	return null;
+
+}
+
+const withSnow = [
+	'abies-grandis',
+	'common-spruces',
+	'conifers',
+	'grand-firs',
+	// Larches don't need to be in this array as they have a different way of 
+	// handling the snow model!
+	// 'larches',
+	'serbian-spruces',
+	'subalpine',
+];
+function hasSnow(pkg) {
+	let name = pkg.split('.').at(1);
+	return withSnow.some(what => name.includes(what));
+}
+
+const db = await run();
+
+const doc = new Document({
+	trees: Object.entries(db).map(([id, tree]) => {
+		return { id, ...tree };
+	}).sort((a, b) => a.id < b.id ? -1 : 1),
+});
+
+let trees = doc.get('trees', true);
+for (let tree of trees.items) {
+	let seasons = tree.get('seasons', true);
+	for (let season of seasons.items) {
+		season.value.flow = true;
+		for (let nr of season.value.items) {
+			nr.format = 'HEX';
+		}
+	}
+}
+
+console.log(doc.toString());
