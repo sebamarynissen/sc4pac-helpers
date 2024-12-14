c0d951cefe69ddf9bb04c2ec07229af08d5b3702
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-14 10:39:18 +0100

    Exclude evergreen flora from girafe's patch

--- scripts/generate-girafe-patch.js
+++ scripts/generate-girafe-patch.js
@@ -1,15 +1,17 @@
 // # generate-girafe-patch.js
 import path from 'node:path';
 import generate from '../lib/generate-tree-patch.js';
-import { getId } from './build-girafe-database.js';
 
 let set = new Set();
 await generate(['{girafe,orange}:*', 'mgb204:girafe-mmp-*'], {
-	id: getId,
 	cwd: path.resolve(import.meta.dirname, '../packages/Girafe'),
 	filter(exemplar, entry) {
 		let { file } = entry.dbpf;
 		if (file.match(/winter/i)) return false;
+		if (file.match(/wheat/i)) return false;
+		if (file.match(/parasol_pines/i)) return false;
+		if (file.match(/cypress/i)) return false;
+		if (file.match(/canary/i)) return false;
 		set.add(file);
 		return true;
 	},
