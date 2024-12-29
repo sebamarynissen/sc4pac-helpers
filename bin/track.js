092ec55746cd51200349e8c88d00a7e46cfab1b3
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-29 17:29:36 +0100

    Remove old tracking script

--- bin/track.js
+++
@@ -1,6 +0,0 @@
-import DependencyTracker from '../lib/dependency-tracker.js';
-
-const tracker = new DependencyTracker();
-const sources = process.argv.slice(2);
-const result = await tracker.track(sources);
-result.dump();
