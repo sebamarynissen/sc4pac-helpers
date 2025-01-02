cfd2d91ab3be636eb736e8bbe7fd3c0e1dcf28a5
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-02 18:58:11 +0100

    Properly track dependencies for mattb commercial

---
+++ scripts/track-commercial-deps.js
@@ -0,0 +1,33 @@
+// # track-sm2-deps.js
+import traverse from '../lib/traverse-yaml.js';
+import { DependencyTracker } from 'sc4/plugins';
+
+const tracker = new DependencyTracker();
+await traverse('**/mattb325/commercial-collection.yaml', async (json, doc) => {
+	
+	if (!json.group) return;
+	if (json.name === 'commercial-collection') return;
+	if (json.name === 'commercial-collection-shared-respources') return;
+	const pkg = `${json.group}:${json.name}`;
+	try {
+		const result = await tracker.track(pkg);
+		let exclude = new Set([
+			'simfox:day-and-nite-mod',
+			'bsc:essentials',
+		]);
+		let deps = result.packages
+			.filter(pkg => !exclude.has(pkg))
+			.sort();
+		if (deps.length > 0) {
+			json.dependencies = deps;
+			return json;
+		} else {
+			console.log(pkg);
+			delete json.dependencies;
+			return json;
+		}
+	} catch (e) {
+		console.log('ERROR in', pkg);
+	}
+
+});
