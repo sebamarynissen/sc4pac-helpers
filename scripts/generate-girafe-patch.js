401c16b2ba5f71b75b4c85e44f02838b6263bdc8
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-02-04 17:16:40 +0100

    Generate patches for t-wrecks replacement mod

--- scripts/generate-girafe-patch.js
+++ scripts/generate-girafe-patch.js
@@ -1,9 +1,11 @@
 // # generate-girafe-patch.js
 import path from 'node:path';
+import { DBPF, FileType } from 'sc4/core';
 import generate from '../lib/generate-tree-patch.js';
+import TreeDatabase from '../lib/tree-database.js';
 
 let set = new Set();
-await generate(['girafe:*', 'mgb204:girafe-mmp-*'], {
+const patches = await generate(['girafe:*', 'mgb204:girafe-mmp-*', 't-wrecks:maxis-tree-hd-replacement-mod'], {
 	cwd: path.resolve(import.meta.dirname, '../packages/Girafe'),
 	filter(exemplar, entry) {
 		let { file } = entry.dbpf;
@@ -16,4 +18,33 @@ await generate(['girafe:*', 'mgb204:girafe-mmp-*'], {
 		return true;
 	},
 	name: 'z_static_season_girafe',
+	labels: true,
+	save: false,
 });
+
+const db = new TreeDatabase();
+await db.load();
+
+// Read in the 3D models from T-Wrecks' patch. We'll have to add them to a 
+// different DBPF under a different tgi - more specifically a different *group*!
+let dbpf = new DBPF(path.resolve(import.meta.dirname, '../packages/Girafe/180-flora/t-wrecks.maxis-tree-hd-replacement-mod.1.0.sc4pac/Maxis Tree HD Replacement.dat'));
+for (let entry of dbpf.findAll({ type: FileType.S3D })) {
+	let [type, group, instance] = entry.tgi;
+	let tree = db.find(tree => {
+		return tree.models.some(({ model }) => {
+			return model[0] === group;
+		});
+	});
+	for (let patch of patches) {
+		let model = db.findSeasonModel(tree.id, patch.season);
+		patch.dbpf.add({
+			type,
+			group: model[0],
+			instance,
+		}, entry.readRaw(), { compressed: entry.compressed });
+	}
+}
+for (let { dbpf, season } of patches) {
+	let fileName = `z_static_season_girafe_${season.toUpperCase()}.dat`;
+	dbpf.save(path.join(process.env.SC4_PLUGINS, '849-my-overrides', fileName));
+}
