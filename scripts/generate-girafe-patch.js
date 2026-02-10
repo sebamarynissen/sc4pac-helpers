bdf668472449616d6d07a5ed301069087fa49551
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2026-02-10 21:07:21 +0100

    Handle `t-wrecks:maxis-tree-hd-replacement-mod`

--- scripts/generate-girafe-patch.js
+++ scripts/generate-girafe-patch.js
@@ -1,8 +1,25 @@
 // # generate-girafe-patch.js
 import path from 'node:path';
+import fs from 'node:fs';
 import { DBPF, FileType } from 'sc4/core';
 import generate from '../lib/generate-tree-patch.js';
 import TreeDatabase from '../lib/tree-database.js';
+import { getCompressionInfo } from 'sc4/utils';
+
+fs.rmSync(String.raw`C:\Users\sebam\Documents\SimCity 4\Plugins\849-my-overrides\smf_16.everseasonal-flora`, {
+	recursive: true,
+	force: true,
+});
+
+const cwd = path.resolve(import.meta.dirname, '../packages/Girafe');
+const opts = {
+	cwd,
+	seasons: ['summer'],
+	// labels: true,
+};
+
+const db = new TreeDatabase();
+await db.load();
 
 // Evergreen flora that does not need to be patched.
 const evergreens = [
@@ -19,12 +36,11 @@ const excludeProps = [
 	'girafe:sparaxis',
 ];
 
-const cwd = path.resolve(import.meta.dirname, '../packages/Girafe');
 await generate([
 	'girafe:*',
 	'orange:*',
 ], {
-	cwd,
+	...opts,
 	filter({ entry, pkg, type }) {
 		let { file } = entry.dbpf;
 
@@ -45,9 +61,10 @@ await generate([
 	labels: true,
 });
 
-// Handle mgb204:mmp-pack-vol2 which adds some of Girafes props as flora as well.
+// Handle mgb204:mmp-pack-vol2 which adds some of Girafes props as flora as 
+// well.
 await generate('mgb204:mmp-pack-vol2', {
-	cwd,
+	...opts,
 	filter({ exemplar }) {
 
 		// Exclude some evergreen trees.
@@ -60,29 +77,47 @@ await generate('mgb204:mmp-pack-vol2', {
 	labels: true,
 });
 
-const db = new TreeDatabase();
-await db.load();
+// Handle t-wrecks:maxis-tree-hd-replacement-mod. Note that this one is a bit 
+// more complicated because it also includes rescaled models that rely on the 
+// original *textures*. In that case, the model instance looks like 0x10030000 
+// instead of 0x00030000, so we filter those out as no model will be found in 
+// the database.
+const patches = await generate('t-wrecks:maxis-tree-hd-replacement-mod', {
+	...opts,
+	filter({ exemplar }) {
+		const name = exemplar.get('ExemplarName');
+		return !name.match(/Palm|_palm_/);
+	},
+});
 
-// Read in the 3D models from T-Wrecks' patch. We'll have to add them to a 
-// different DBPF under a different tgi - more specifically a different *group*!
-// let dbpf = new DBPF(path.resolve(import.meta.dirname, '../packages/Girafe/180-flora/t-wrecks.maxis-tree-hd-replacement-mod.1.0.sc4pac/Maxis Tree HD Replacement.dat'));
-// for (let entry of dbpf.findAll({ type: FileType.S3D })) {
-// 	let [type, group, instance] = entry.tgi;
-// 	let tree = db.find(tree => {
-// 		return tree.models.some(({ model }) => {
-// 			return model[0] === group;
-// 		});
-// 	});
-// 	for (let patch of patches) {
-// 		let model = db.findSeasonModel(tree.id, patch.season);
-// 		patch.dbpf.add({
-// 			type,
-// 			group: model[0],
-// 			instance,
-// 		}, entry.readRaw(), { compressed: entry.compressed });
-// 	}
-// }
-// for (let { dbpf, season } of patches) {
-// 	let fileName = `z_static_season_girafe_${season.toUpperCase()}.dat`;
-// 	dbpf.save(path.join(process.env.SC4_PLUGINS, '849-my-overrides', fileName));
-// }
+// Read in the 3D models form T-Wreck's patch. We'll have to add them to the 
+// patch under a different tgi - more specifically a different *group*.
+const dbpf = new DBPF(path.join(cwd, '180-flora/t-wrecks.maxis-tree-hd-replacement-mod.1.0.1.sc4pac/Maxis Tree HD Replacement.dat'));
+for (let entry of dbpf.findAll({ type: FileType.S3D })) {
+	const [type, group, instance] = entry.tgi;
+	const model = [group, 0x10030000];
+	const raw = entry.readRaw();
+	const { compressed } = getCompressionInfo(raw);
+	const [family] = db.findMatchingFamilies({
+		model,
+		type: 'prop',
+		pkg: 't-wrecks:maxis-tree-hd-replacement-mod',
+	});
+	// If the family was not found, we're dealing with a non-seasonal tree (
+	// i.e. the palms).
+	if (!family) continue;
+	for (let season of opts.seasons) {
+		const [newGroup] = db.findSeasonModelFromFamily(family, season);
+		// If the group hasn't changed, no need to do anything. This is the case for summer and spring normally, and for the palms.
+		if (group === newGroup) continue;
+		const { dbpf } = patches.find(patch => patch.season === season);
+		dbpf.add({
+			type,
+			group: newGroup,
+			instance,
+		}, raw, { compressed });
+	}
+}
+for (const { dbpf, fullPath } of patches) {
+	await dbpf.save(fullPath);
+}
